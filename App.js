08d4cdaa44be43024a480a0a3155ae54a8414eba
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-06-18 00:22:58 -0400

    imported firebase

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 
+import * as firebase from 'firebase';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
