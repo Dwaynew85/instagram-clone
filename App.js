56a2cc21473a8d5141de88a88065371cc9435353
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-06-19 01:16:58 -0400

    added registration screen

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
 
 const Stack = createStackNavigator();
 export default function App() {
@@ -19,6 +20,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Landing">
         <Stack.Screen name="Landing" component={LandingScreen} options={{headerShown: false }}/>
+        <Stack.Screen name="Register" component={RegisterScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
