66e34ce9132a5debb242808d84855367b204b3e5
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-06-21 01:30:35 -0400

    added firebaseConfig

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { FIREBASE_CONFIG } from "react-native-dotenv"
-
 import firebase from 'firebase';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyC-zX_rqEu8swNEbkP_QO_50aLA1d0dtHY",
+  authDomain: "instagram-dev-723ae.firebaseapp.com",
+  projectId: "instagram-dev-723ae",
+  storageBucket: "instagram-dev-723ae.appspot.com",
+  messagingSenderId: "898788509905",
+  appId: "1:898788509905:web:42d5ca61bdff1852ef5df0",
+  measurementId: "G-5B20G7C95C"
+};
+
 if(firebase.apps.length === 0){
-  firebase.initializeApp(FIREBASE_CONFIG)
+  firebase.initializeApp(firebaseConfig)
 }
 
 import { NavigationContainer } from '@react-navigation/native';
