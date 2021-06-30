3ab9611fe4c8ffc2ee5a41d2dcb62f5aa5b293b9
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-06-30 00:03:39 -0400

    added user state change constant and log if user doesn't exist

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,3 +1,4 @@
+import { USER_STATE_CHANGE } from '../constants/index';
 import firebase from "firebase";
 
 export function fetchUser() {
@@ -9,6 +10,8 @@ export function fetchUser() {
             .then((snapshot) => {
                 if(snapshot.exists){
                     dispatch({type : USER_STATE_CHANGE, currentUser: snapshot.data()})
+                } else {
+                    console.log('does not exist')
                 }
             })
     })
