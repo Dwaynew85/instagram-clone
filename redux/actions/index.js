03ea02485370bc86bcf6995766d2efa22079b3de
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-07-01 23:33:19 -0400

    fixed syntax uses and collection call error

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,16 +1,18 @@
 import { USER_STATE_CHANGE } from '../constants/index';
-import firebase from "firebase";
+import firebase from 'firebase';
 
 export function fetchUser() {
-    return((dispatch) => {
+    return ((dispatch) => {
         firebase.firestore()
-            .collection("user")
+            .collection("users")
             .doc(firebase.auth().currentUser.uid)
             .get()
             .then((snapshot) => {
                 if(snapshot.exists){
+                    console.log(snapshot.data())
                     dispatch({type : USER_STATE_CHANGE, currentUser: snapshot.data()})
-                } else {
+                }
+                else{
                     console.log('does not exist')
                 }
             })
